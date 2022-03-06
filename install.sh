a48a8fdef97b7206f4888493a772ece29814a54d
ryo.nakao <ryo.nakao@woven-planet.global>
2022-03-06 17:59:25 +0900

    Organiza what we install

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # brew
-tap=("caskroom/fonts")
-brew=("tmux" "ripgrep" "go" "fzf" "python3" "rbenv" "ruby-build" "direnv" "desk" "hugo" "tree" "jq" "pt" "htop" "starship" "denisidoro/tools/navi" "hub" "golangci/tap/golangci-lint" "ghq" "jesseduffield/lazygit/lazygit" "bash-completion" "c-bata/kube-prompt/kube-prompt")
-cask=("iterm2" "font-fira-code" "kap")
+brew=("tmux" "pyenv" "ripgrep" "go" "fzf" "python3" "direnv" "hugo" "tree" "jq" "pt" "htop" "starship" "denisidoro/tools/navi" "hub" "golangci/tap/golangci-lint" "ghq" "jesseduffield/lazygit/lazygit" "bash-completion" "c-bata/kube-prompt/kube-prompt" "caskroom/fonts/fonts")
+cask=("iterm2" "font-fira-code")
 if type "brew"; then
   echo "$(tput setaf 2)Already installed Homebrew ✔︎$(tput sgr0)"
 else
@@ -11,9 +10,6 @@ else
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 if type "tap"; then
-  for b in ${tap[@]}; do
-    brew tap $b
-  done
   for b in ${brew[@]}; do
     brew install $b
   done
@@ -23,41 +19,16 @@ if type "tap"; then
 
 fi
 
-# git
-if type "git"; then
-  echo "$(tput setaf 2)Already installed Git ✔︎$(tput sgr0)"
-else
-  echo "Installing Git..."
-  brew install git
-fi
-if type "git"; then
-  git clone https://github.com/riywo/anyenv ~/.anyenv
-  git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-fi
-
-# go
-if type "go"; then
-  echo "$(tput setaf 2)Already installed Go ✔︎$(tput sgr0)"
-else
-  echo "Installing Go..."
-  brew install go
-fi
-if type "go"; then
-  go get github.com/rogpeppe/gohack
-fi
-
-
 # ghq
 if type "ghq"; then
   echo "$(tput setaf 2)Already installed ghq ✔︎$(tput sgr0)"
 else
   echo "Installing ghq..."
-  go get github.com/motemen/ghq
+    brew install ghq
 fi
 if type "ghq"; then
   ghq get https://github.com/b4b4r07/enhancd
 fi
-
 
 # pip3
 if type "pip3"; then
@@ -72,6 +43,7 @@ if type "pip3"; then
 fi
 
 # curl
+mkdir -p ~/.config/nvim/
 cd ~/.config/nvim/
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.config/nvim/
